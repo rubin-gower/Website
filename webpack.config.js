00d64ef6293b125987e28fd8254e07beb417d87a
rubin-gower <rubinsemails@gmail.com>
2020-11-07 23:26:23 +1300

    build now creates file

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,8 @@ const path = require('path')
 module.exports = {
   entry: './client/index.js',
   output: {
-    path: path.join(__dirname, 'server/public'),
-    filename: 'bundle.js'
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'build'),
   },
   mode: 'development',
   module: {
